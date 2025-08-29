8e5059950caa1f81893504129dffe1afcc6a6bf2
aggviolinist <mulandimaei76@gmail.com>
2025-08-29 11:18:50 +0300

    repo update

---
+++ src/main/java/com/drivefundproject/drive_fund/model/User.java
@@ -0,0 +1,70 @@
+package com.drivefundproject.drive_fund.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user-table")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+
+    private String imageUrl;
+
+    
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
+        return email;
+
+    }
+
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
+        return password;
+    }
+    
+}
---
+++ src/main/java/com/drivefundproject/drive_fund/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.drivefundproject.drive_fund.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.drivefundproject.drive_fund.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+     Optional<User> findByEmail(String email);
+}
