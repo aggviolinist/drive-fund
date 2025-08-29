bd5d7cf9ad595bac4edeb0db15a2c26edc089cfd
aggviolinist <mulandimaei76@gmail.com>
2025-08-29 10:12:15 +0300

    drive fund init

---
+++ src/main/java/com/drivefundproject/drive_fund/DriveFundApplication.java
@@ -0,0 +1,13 @@
+package com.drivefundproject.drive_fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveFundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drivefundproject/drive_fund/DriveFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivefundproject.drive_fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
