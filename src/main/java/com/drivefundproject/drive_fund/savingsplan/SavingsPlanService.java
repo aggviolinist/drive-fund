259fdd1117986a4641c7bd4065e964e3ef2398f7
sCuLpToR <72200097+aggviolinist@users.noreply.github.com>
2025-09-04 13:31:35 +0000

    savings plan

---
+++ src/main/java/com/drivefundproject/drive_fund/savingsplan/SavingsPlanService.java
@@ -0,0 +1,42 @@
+package com.drivefundproject.drive_fund.savingsplan;
+
+import org.springframework.stereotype.Service;
+
+import com.drivefundproject.drive_fund.dto.Request.SavingsPlanRequest;
+import com.drivefundproject.drive_fund.model.Catalogue;
+import com.drivefundproject.drive_fund.model.SavingsPlan;
+import com.drivefundproject.drive_fund.model.User;
+import com.drivefundproject.drive_fund.repository.CatalogueRepository;
+import com.drivefundproject.drive_fund.repository.SavingsPlanRepository;
+import com.drivefundproject.drive_fund.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class SavingsPlanService  {
+
+    private final SavingsPlanRepository savingsPlanRepository;
+    private final CatalogueRepository catalogueRepository;
+    private final UserRepository userRepository;
+
+    public SavingsPlan addSavingsPlan(SavingsPlanRequest savingsPlanRequest, Integer userId){
+        Catalogue catalogueItem = catalogueRepository.findById(savingsPlanRequest.getProductID())
+            .orElseThrow(() -> new RuntimeException("Product not found"));
+
+
+        User user = userRepository.findById(userId) 
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+        SavingsPlan savingsPlan = SavingsPlan.builder()
+             .timeline(savingsPlanRequest.getTimeline())
+             .amount(savingsPlanRequest.getAmount())
+             .catalogue(catalogueItem)
+             .user(user)
+             .build();
+        //save user in SavingsPlan table
+        return savingsPlanRepository.save(savingsPlan);
+
+    }  
+}
