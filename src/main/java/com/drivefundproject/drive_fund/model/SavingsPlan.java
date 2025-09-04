366deb1e4f0d010097fd0939699d8b01b2a758bc
sCuLpToR <72200097+aggviolinist@users.noreply.github.com>
2025-09-04 10:02:43 +0000

    savings plan

--- src/main/java/com/drivefundproject/drive_fund/model/SavingsPlan.java
+++ src/main/java/com/drivefundproject/drive_fund/model/SavingsPlan.java
@@ -1,8 +1,12 @@
 package com.drivefundproject.drive_fund.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,6 +28,14 @@ public class SavingsPlan {
     private Double amount;
     private String timeline; 
     
-    //Establishing a many to one relationship between catalogue and savingsplan
+    //Establishing a many to one relationship between savingsplan and catalogue,user 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "catalogue_id")
+    private Catalogue catalogue ;
+
+    //The user who made the savings plan
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
 }
---
+++ src/main/java/com/drivefundproject/drive_fund/repository/SavingsPlanRepository.java
@@ -0,0 +1,11 @@
+package com.drivefundproject.drive_fund.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.drivefundproject.drive_fund.model.SavingsPlan;
+
+@Repository
+public interface SavingsPlanRepository extends JpaRepository<SavingsPlan, Integer> {
+    
+}
