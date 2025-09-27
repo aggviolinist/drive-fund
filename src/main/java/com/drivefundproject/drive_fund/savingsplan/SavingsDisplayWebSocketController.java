cb219492ca05ae3fbe8577db019032b8651b2dbd
aggviolinist <mulandimaei76@gmail.com>
2025-09-27 10:35:58 +0300

    planUuid update

--- src/main/java/com/drivefundproject/drive_fund/savingsplan/SavingsDisplayWebSocketController.java
+++ src/main/java/com/drivefundproject/drive_fund/savingsplan/SavingsDisplayWebSocketController.java
@@ -3,11 +3,13 @@ package com.drivefundproject.drive_fund.savingsplan;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.drivefundproject.drive_fund.dto.Request.SocketPaymentRequest;
@@ -28,16 +30,17 @@ public class SavingsDisplayWebSocketController {
     private final SavingsDisplayService savingsDisplayService;
     private final ObjectMapper objectMapper; //Jackson json-java object, java object-json
 
-    private final UUID MOCK_SAVINGS_PLAN_UUID = UUID.fromString("db6dab4e-6524-4a84-8824-2ab2c472303e"); //TO DO
+    //private final UUID MOCK_SAVINGS_PLAN_UUID = UUID.fromString("db6dab4e-6524-4a84-8824-2ab2c472303e"); //TO DO
 
-     @GetMapping("/messages")
-    public String getTemplate(Model model){
+     @GetMapping("/messages/{planUuid}")
+    public String getTemplate(@PathVariable UUID planUuid, Model model){
+        model.addAttribute("planUuid", planUuid.toString());
         return "index";
-    }
+    } //To be Deleted
     
-    @MessageMapping("/deposit")
-    @SendTo("/topic/progress")
-    public String handleDepositAndCalculateProgress(String message) throws JsonProcessingException{
+    @MessageMapping("/deposit/{planUuid}")
+    @SendTo("/topic/progress/{planUuid}")
+    public String handleDepositAndCalculateProgress(@DestinationVariable UUID planUuid, String message) throws JsonProcessingException{
         SocketPaymentRequest depositRequest;
 
         try{
@@ -48,12 +51,12 @@ public class SavingsDisplayWebSocketController {
             return "{\"error\": \"Invalid deposit format or missing amount.\"}";
         }
         BigDecimal depositAmount = depositRequest.getAmount();
-       // System.out.println("Received deposit of:" + depositAmount + "for Plan: " + MOCK_SAVINGS_PLAN_UUID);
+       System.out.println("Received deposit of:" + depositAmount + "for Plan: " + planUuid);
 
         try{
-            Payment newPayment = paymentService.recordPaymentDeposit(MOCK_SAVINGS_PLAN_UUID, depositAmount);
+            Payment newPayment = paymentService.recordPaymentDeposit(planUuid, depositAmount);
             
-            SavingsProgressResponse savingsProgressResponse = savingsDisplayService.getSavingsProgress(MOCK_SAVINGS_PLAN_UUID);
+            SavingsProgressResponse savingsProgressResponse = savingsDisplayService.getSavingsProgress(planUuid);
 
             PaymentResponse paymentResponse = savingsDisplayService.createPaymentResponse(newPayment);
 
