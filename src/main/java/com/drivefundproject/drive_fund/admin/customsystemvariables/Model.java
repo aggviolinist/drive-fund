f486eac429826dec60c9862c87968c0057c8c9c0
sCuLpToR <72200097+aggviolinist@users.noreply.github.com>
2025-10-20 08:40:34 +0000

    model update variables

---
+++ src/main/java/com/drivefundproject/drive_fund/admin/customsystemvariables/Model.java
@@ -0,0 +1,23 @@
+package com.drivefundproject.drive_fund.admin.customsystemvariables;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String key;
+
+    private BigDecimal value;
+    
+}
