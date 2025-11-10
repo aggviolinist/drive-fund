28744315abd93283b79c3240d38ea07ebf5676f4
aggviolinist <mulandimaei76@gmail.com>
2025-11-10 17:52:50 +0300

    response on custom variables

--- src/main/java/com/drivefundproject/drive_fund/admin/customsystemvariables/dto/response/GetAllCustomVariablesResponse.java
+++ src/main/java/com/drivefundproject/drive_fund/admin/customsystemvariables/dto/response/GetAllCustomVariablesResponse.java
@@ -1,5 +1,13 @@
 package com.drivefundproject.drive_fund.admin.customsystemvariables.dto.response;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Data
 @Builder
 @NoArgsConstructor
